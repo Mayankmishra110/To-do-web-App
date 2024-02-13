0e208b80bdd055d9cd4d37fed35823e87d8bd1fd
Mayankmishra110 <kmmay15@gmail.com>
2024-02-13 23:11:39 +0530

    Frontend done

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const { createTodo } = require("/types");
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.post("/todo", async function (req, res) {
   const createPayload = req.body;
   const parsePayload = createTodo.safeParse(createPayload);
 
-  if (!parsePayload.sucess) {
+  if (!parsePayload.success) {
     res.status(411).json({
       msg: "you sent the wrong inputs",
     });
@@ -34,7 +35,7 @@ app.get("/todos", async function (req, res) {
   });
 });
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
   const updatePayload = req.body;
   const parsePayload = update.safeParse(updatePayload);
   if (!parsePayload.sucess) {
@@ -43,4 +44,18 @@ app.put("/completed", function (req, res) {
     });
     return;
   }
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
+
+app.listen(8000);
